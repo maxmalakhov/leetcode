ea11f39f952cb19b89468eefc3bf95eeab527e77
Max M <maksim.malakhov@softeq.com>
2022-04-06 22:13:34 +0300

    Added simple HashMap solution

---
+++ src/main/java/max/leetcode/easy/MyHashMap.java
@@ -0,0 +1,75 @@
+package max.leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * Input:
+ *  ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+ *  [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+ *
+ * Output:
+ *  [null, null, null, 1, -1, null, 1, null, -1]
+ *
+ * Constraints:
+ *  0 <= key, value <= 10^6
+ *  At most 10^4 calls will be made to put, get, and remove.
+ */
+public class MyHashMap {
+
+    private static final int SIZE = 1000;
+    private int[] hashtable = new int[SIZE];
+
+    public MyHashMap() {
+        init(0);
+    }
+
+    public void put(int key, int value) {
+        if (key >= hashtable.length) {
+            resize(key);  // length 1000, key == 3000
+        }
+        hashtable[key] = value;
+    }
+
+    public int get(int key) {
+        return key < hashtable.length ? hashtable[key] : -1;
+    }
+
+    public void remove(int key) {
+        if (key < hashtable.length) {
+            hashtable[key] = -1;
+        }
+    }
+
+    public void printout() {
+        for (int i = 0; i < hashtable.length; i++) {
+            System.out.print(hashtable[i] + " ");
+        }
+    }
+
+    private void resize(int key) {
+        int oldSize = hashtable.length;
+        int newSize = (key + SIZE) / SIZE * SIZE - oldSize;
+        hashtable = Arrays.copyOf(hashtable, oldSize + newSize);
+        init(oldSize);
+    }
+
+    private void init(int start) {
+        for (int i = start; i < hashtable.length; i++) {
+            hashtable[i] = -1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MyHashMap solution = new MyHashMap();
+
+        solution.put(1, 1);
+        solution.put(1000, 1000);
+        solution.put(1001, 1001);
+        solution.put(4001, 4001);
+
+        solution.remove(1001);
+
+        solution.printout();
+    }
+}

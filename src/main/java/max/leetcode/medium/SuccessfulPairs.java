7a2ed04f094bab54b883edf1c674672b511aa270
Max M <malakhovbox@gmail.com>
2023-04-03 00:00:43 +0300

    Improved solution for SuccessfulPairs via BinarySearch

--- src/main/java/max/leetcode/medium/SuccessfulPairs.java
+++ src/main/java/max/leetcode/medium/SuccessfulPairs.java
@@ -9,21 +9,33 @@ public class SuccessfulPairs {
 
         SuccessfulPairs java = new SuccessfulPairs();
 
-        System.out.println(" [5,1,3] * [1,2,3,4,5] >= 7 " + Arrays.toString(java.successfulPairs(new int[] {5,1,3}, new int [] {1,2,3,4,5}, 7)) + "' == '[3.0.4]'");
-        System.out.println(" [3,1,2] * [8,5,8] >= 16 " + Arrays.toString(java.successfulPairs(new int[] {3,1,2}, new int [] {8,5,8}, 16)) + "' == '[2.0.2]'");
+        System.out.println("# [5,1,3] * [1,2,3,4,5] >= 7 -- " + Arrays.toString(java.successfulPairs(new int[] {5,1,3}, new int [] {1,2,3,4,5}, 7)) + "' == '[4, 0, 3]'");
+        System.out.println("# [3,1,2] * [8,5,8] >= 16 -- " + Arrays.toString(java.successfulPairs(new int[] {3,1,2}, new int [] {8,5,8}, 16)) + "' == '[2, 0, 2]'");
+        System.out.println("# [15,8,19] * [38,36,23] >= 328 -- " + Arrays.toString(java.successfulPairs(new int[] {15,8,19}, new int [] {38,36,23}, 328)) + "' == '[3, 0, 3]'");
     }
 
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        int[] pairs = new int[spells.length];
-        for (int i = 0; i < spells.length; i++) {
-            int count = 0;
-            for (int j = 0; j < potions.length; j++) {
-                if ((long) spells[i] * potions[j] >= success) {
-                    count++;
+        Arrays.sort(potions);
+        for (int i = 0; i < spells.length; i++) { // O(n)
+            int left = 0, right = potions.length - 1;
+            int idx = Integer.MAX_VALUE;
+            while (left <= right) { // O(logn)
+                int pos = (left + right) / 2;
+                long current = (long) potions[pos] * spells[i];
+                System.out.println(". spell = " + spells[i] + ", potions[" + pos + "] = " + potions[pos] + ", current = " + current + ", success = " + success);
+                if (current >= success) {
+                    idx = Math.min(idx, pos);
+                    right = pos - 1;
+                } else {
+                    left = pos + 1;
                 }
             }
-            pairs[i] = count;
+            spells[i] = idx < Integer.MAX_VALUE ? potions.length - idx : 0;
+//            for (int j = potions.length - 1; j >= 0 && (long) potions[j] * spell >= success; j--) { // O(n/2)
+//                System.out.println(". spell = " + spells[i] + ", potion = " + potions[j] + ", sum = " + (long) potions[j] * spell);
+//                spells[i]++;
+//            }
         }
-        return pairs;
+        return spells;
     }
 }
